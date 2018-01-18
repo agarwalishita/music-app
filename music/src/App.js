44a150cfc2318f3c313fd4b7d068f1f0aa95be24
agarwalishita <ishita.nishu1999@gmail.com>
2018-01-18 20:29:36 +0530

    problems!

---
+++ music/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import radioPage from './pages/radioPage';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <radioPage/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ music/src/pages/radioPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import radioUpper from '../components/radioUpper'
+export default class radioPage extends Component {
+    render(){
+        return
+          (
+            <div>
+                <radioUpper/>
+             </div>
+
+        )
+    }
+}
